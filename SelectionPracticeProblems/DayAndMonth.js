c509134eb1f62b8aa08e71bac0c1f8a0f4528229
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-23 00:19:03 +0530

    [Ravina] Added : Checking day and month between march and june .

---
+++ SelectionPracticeProblems/DayAndMonth.js
@@ -0,0 +1,20 @@
+"use strict";
+
+ const ps = require("prompt-sync");
+ const prompt = ps();
+
+ let day = prompt("Enter day  :");
+ let month = prompt("Enter month  :");
+
+ if ( ( month >= 3 && day >= 20 )  && ( month <= 6 && day <= 20 )  && ( day < 31 ) )
+ {
+     console.log("True");
+
+ }
+ else
+ {
+     console.log("False");
+ }
+
+
+ 
